699db67cca24bdfabe12b3ed3315a48b325929d2
Avinoorsingh <avinoorsingh123@gmail.com>
2025-02-13 20:46:39 -0500

    first commit

---
+++ Minmax.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+   let sumValue=arr.reduce((a ,b)=>{
+    return a+b;
+   })
+   let maxValue=sumValue-Math.min(...arr);
+   let minValue=sumValue-Math.max(...arr);
+   console.log(minValue+" "+maxValue);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
